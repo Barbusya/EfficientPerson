ebbb3fe9aae2b470de7254084408aa2f8c6f9cc3
bulat <bbbrrr8877@gmail.com>
2023-02-07 14:42:06 +0300

    HabitListAdapter.kt

--- app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitlist/HabitListAdapter.kt
+++ app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitlist/HabitListAdapter.kt
@@ -3,17 +3,27 @@ package com.bbbrrr8877.efficientperson.habits.presentation.habitlist
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bbbrrr8877.efficientperson.R
 import com.bbbrrr8877.efficientperson.habits.domain.Etities.HabitItem
 
-class HabitListAdapter : ListAdapter<HabitItem, HabitItemViewHolder>(HabitItemDiffCallback()) {
+class HabitListAdapter : RecyclerView.Adapter<HabitItemViewHolder>() {
+
+    val list = listOf<HabitItem>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitItemViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_habit_good_disabled, parent, false)
+        return HabitItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: HabitItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = list.size
 
+    override fun onBindViewHolder(viewHolder: HabitItemViewHolder, position: Int) {
+        val habitItem = list[position]
+        viewHolder.tvTitle.text = habitItem.title
+        viewHolder.cbPassedOrNot.isChecked = habitItem.isDone
+        viewHolder.view.setOnClickListener {
+            true
+        }
+    }
 
 }
