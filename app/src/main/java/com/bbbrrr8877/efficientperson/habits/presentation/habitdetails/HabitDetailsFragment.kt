3715a5f1650a5de3cd3c8c3d585d188ab20a51e4
bulat <bbbrrr8877@gmail.com>
2023-02-08 10:47:08 +0300

    HabitDetailsViewModel.kt

--- app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitdetails/HabitDetailsFragment.kt
+++ app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitdetails/HabitDetailsFragment.kt
@@ -1,19 +1,15 @@
 package com.bbbrrr8877.efficientperson.habits.presentation.habitdetails
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.bbbrrr8877.efficientperson.R
 import com.bbbrrr8877.efficientperson.databinding.FragmentHabitDetailsBinding
-import com.bbbrrr8877.efficientperson.habits.domain.Etities.HabitItem
 
 class HabitDetailsFragment : Fragment() {
+
+    private lateinit var viewModel: HabitDetailsViewModel
 
     private var _binding: FragmentHabitDetailsBinding? = null
     private val binding: FragmentHabitDetailsBinding
@@ -30,7 +26,6 @@ class HabitDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitdetails/HabitDetailsViewModel.kt
+++ app/src/main/java/com/bbbrrr8877/efficientperson/habits/presentation/habitdetails/HabitDetailsViewModel.kt
@@ -1,11 +1,15 @@
 package com.bbbrrr8877.efficientperson.habits.presentation.habitdetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bbbrrr8877.efficientperson.habits.data.room.HabitRepositoryImpl
 import com.bbbrrr8877.efficientperson.habits.domain.Etities.HabitItem
-import com.bbbrrr8877.efficientperson.habits.domain.usecases.*
+import com.bbbrrr8877.efficientperson.habits.domain.usecases.AddHabitItemUseCase
+import com.bbbrrr8877.efficientperson.habits.domain.usecases.EditHabitItemUseCase
+import com.bbbrrr8877.efficientperson.habits.domain.usecases.GetHabitItemUseCase
 
-class HabitDetailsViewModel: ViewModel() {
+class HabitDetailsViewModel : ViewModel() {
 
     private val repository = HabitRepositoryImpl
 
@@ -13,8 +17,19 @@ class HabitDetailsViewModel: ViewModel() {
     private val addHabitListUseCase = AddHabitItemUseCase(repository)
     private val editHabitListUseCase = EditHabitItemUseCase(repository)
 
+    private val _errorInputTitle = MutableLiveData<Boolean>()
+    val errorInputTitle: LiveData<Boolean> = _errorInputTitle
+
+    private val _habitItem = MutableLiveData<HabitItem>()
+    val habitItem: LiveData<HabitItem> = _habitItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
     fun getHabitItem(habitItemId: Long) {
         val item = getHabitItemUseCase.getHabitItem(habitItemId)
+        _habitItem.value = item
     }
 
     fun addHabitItem(
@@ -34,29 +49,30 @@ class HabitDetailsViewModel: ViewModel() {
                 isDone = inputProgress
             )
             addHabitListUseCase.addHabitItem(habitItem)
+            finishWork()
         }
     }
 
     fun editHabitItem(
         inputTitle: String?,
         inputDescription: String?,
-        inputQuality: Boolean,
         inputProgress: Boolean
     ) {
         val title = parseTitle(inputTitle)
         val description = parseDescription(inputDescription)
         val fieldValid = validateInput(title)
         if (fieldValid) {
-            val habitItem = HabitItem(
-                title = title,
-                description = description,
-                isGood = inputQuality,
-                isDone = inputProgress
-            )
-            editHabitListUseCase.editHabitItem(habitItem)
+            _habitItem.value?.let {
+                val item = it.copy(
+                    title = title,
+                    description = description,
+                    isDone = inputProgress
+                )
+                editHabitListUseCase.editHabitItem(item)
+                finishWork()
+            }
         }
     }
-
 
 
     private fun parseTitle(inputTitle: String?): String {
@@ -70,9 +86,17 @@ class HabitDetailsViewModel: ViewModel() {
     private fun validateInput(title: String): Boolean {
         var result = true
         if (title.isBlank()) {
-            // TODO: show error input name
+                _errorInputTitle.value = true
             result = false
         }
         return result
+    }
+
+    private fun resetErrorInputTitle() {
+        _errorInputTitle.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
